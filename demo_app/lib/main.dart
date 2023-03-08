20bb401d9b327054b7c0baa51b473e07c4e66ed7
Safna905 <eksafna786@gmail.com>
2023-03-08 21:02:11 +0530

    up 1

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'Screens/ListTile.dart';
 import 'Screens/ListTileBuilder.dart';
+import 'Screens/SplashScreen.dart';
 import 'Screens/starReviews.dart';
 
 void main () {
@@ -22,7 +24,26 @@ class MyApp extends StatelessWidget {
       // home: StarRatingPage(
       //   title : 'Star Rating'
       // ),
-      home: ListTileBuilderPage(),
+      home: AnimatedSplashScreen(
+        splash:Container(
+          child: Image.network("https://img.icons8.com/color/200w/flutter.png",
+        ),
+      ),
+        duration: 3000,
+
+        // bottomNavigationBar: Container(
+        //   color: Colors.black,
+        //   child: Text('Flutter App',
+        //     style: TextStyle(
+        //         fontWeight: FontWeight.bold,
+        //         fontSize: 45,
+        //         color: Colors.white
+        //     ),),
+        // ),
+        nextScreen: ListTilePage(),
+        splashTransition: SplashTransition.fadeTransition,
+      ),
+
     );
   }
 }
